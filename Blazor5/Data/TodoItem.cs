831a9a9d7ec64781235a69ce687d70e882696347
Adrien Baumann <adrien.baumann434@gmail.com>
2020-06-11 09:52:18 -0500

    Add project files.

---
+++ Blazor5/Data/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor5.Data
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
---
+++ Blazor5/Data/TodoService.cs
@@ -0,0 +1,34 @@
+﻿using Blazor5.Pages;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Blazor5.Data
+{
+    public class TodoService
+    {
+
+        public async Task<TodoItem[]> GetTodosAsync()
+        {
+            using (System.IO.FileStream fs = System.IO.File.OpenRead("Data/json/todos.json"))
+            {
+                var jsonizedData = await JsonSerializer.DeserializeAsync<TodoItem[]>(fs);
+                return jsonizedData;
+            }
+        }
+
+        public async Task UpdateTodosAsync(IList<TodoItem> list)
+        {
+            Console.WriteLine("Doesn't work rn lol");
+            using (System.IO.FileStream fs = System.IO.File.Create("Data/json/todos.json"))
+            {
+                await Task.FromResult(JsonSerializer.SerializeAsync<IList<TodoItem>>(fs, list));
+                //    return jsonizedData;
+            }
+        }
+
+    }
+}
